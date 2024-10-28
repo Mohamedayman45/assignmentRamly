dca105a4dc6cac14f914f09c14bad3551e5c927b
yousefashraf35 <162374854+yousefashraf35@users.noreply.github.com>
2024-10-28 18:45:04 +0300

    Update machine.cpp

--- machine.cpp
+++ machine.cpp
@@ -5,4 +5,18 @@
 #include "machine.h"
 #include <bits/stdc++.h>
 
-
+void machine :: load1(int r, int xy){
+    cout << "load 1";
+    cout << "r is " <<r << endl;
+    cout << "xy is " << xy << endl;
+}
+void machine ::load2(int r, int xy) {
+    cout << "load 2";
+    cout << "r is " <<r << endl;
+    cout << "xy is " << xy << endl;
+}
+void machine ::store(int r, int xy) {
+    cout << "store";
+    cout << "r is " <<r << endl;
+    cout << "xy is " << xy << endl;
+}
