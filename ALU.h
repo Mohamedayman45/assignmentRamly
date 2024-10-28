c0b26b39e90cd193b2cc2f925e42d4a5c7b51d56
yousefashraf35 <162374854+yousefashraf35@users.noreply.github.com>
2024-10-28 18:44:06 +0300

    Update ALU.h

--- ALU.h
+++ ALU.h
@@ -1,1 +1,18 @@
-//hii
+//
+// Created by User on 10/28/2024.
+//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef A1_T4_MAIN_ALU_H
+#define A1_T4_MAIN_ALU_H
+
+
+class ALU {
+    public:
+        int hextodec(string hex);
+};
+
+
+#endif //A1_T4_MAIN_ALU_H
