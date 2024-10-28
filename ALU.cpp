35be8bf9741be5edfb13a07b950c6534e40c435a
yousefashraf35 <162374854+yousefashraf35@users.noreply.github.com>
2024-10-28 18:43:47 +0300

    Update ALU.cpp

--- ALU.cpp
+++ ALU.cpp
@@ -1,18 +1,9 @@
 //
 // Created by User on 10/28/2024.
 //
-#include <bits/stdc++.h>
 
-using namespace std;
+#include "ALU.h"
 
-#ifndef A1_T4_MAIN_ALU_H
-#define A1_T4_MAIN_ALU_H
-
-
-class ALU {
-    public:
-        int hextodec(string hex);
-};
-
-
-#endif //A1_T4_MAIN_ALU_H
+int ALU ::hextodec(string hex) {
+    return stoi(hex, nullptr, 16);
+}
