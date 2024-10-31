f59a9bae645a6467284a7d91d68b7d7ef3a72e6f
yousefashraf35 <162374854+yousefashraf35@users.noreply.github.com>
2024-11-01 00:56:27 +0200

    Update machine.cpp

--- machine.cpp
+++ machine.cpp
@@ -5,15 +5,13 @@
 #include "machine.h"
 #include <bits/stdc++.h>
 
-void machine :: load1(int r, int xy){
-    cout << "load 1";
-    cout << "r is " <<r << endl;
-    cout << "xy is " << xy << endl;
+void machine :: load1(int r, string value) {
+    MemoryRegister get;
+    get.setregister(r, value);
 }
-void machine ::load2(int r, int xy) {
-    cout << "load 2";
-    cout << "r is " <<r << endl;
-    cout << "xy is " << xy << endl;
+void machine ::load2(int r, string valuee) {
+    MemoryRegister set;
+    set.setregister(r ,valuee);
 }
 void machine ::store(int r, int xy) {
     cout << "store";
