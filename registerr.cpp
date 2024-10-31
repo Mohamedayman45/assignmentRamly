1da2e092d65888d6086fbe87aee9855d00559cc8
yousefashraf35 <162374854+yousefashraf35@users.noreply.github.com>
2024-11-01 00:57:36 +0200

    Update registerr.cpp

--- registerr.cpp
+++ registerr.cpp
@@ -3,4 +3,24 @@
 //
 
 #include "registerr.h"
+#include "memory.h"
+#include "machine.h"
 #include <bits/stdc++.h>
+
+vector<string> MemoryRegister::registers(16, "00");  // Initialize once
+
+MemoryRegister::MemoryRegister() {
+}
+
+void MemoryRegister ::setregister(int adress, string valuee)
+{
+    registers[adress]=valuee;
+}
+string MemoryRegister ::getregister(int adress) {
+    return registers[adress];
+}
+int MemoryRegister ::showregisterr() {
+    for (int i=0;i<registers.size();++i){
+        cout <<"register " << i << " equall to " << registers[i] << endl;
+    }
+}
