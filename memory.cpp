eb42ecbd3a06c39eb98f80a8ec9e64513c1414ef
yousefashraf35 <162374854+yousefashraf35@users.noreply.github.com>
2024-11-01 00:55:58 +0200

    Update memory.cpp

--- memory.cpp
+++ memory.cpp
@@ -3,32 +3,40 @@
 //
 #include "memory.h"
 #include "machine.h"
+#include "registerr.h"
 #include "ALU.h"
 #include <bits/stdc++.h>
 
 using namespace std;
-vector <string> memory::ReadToVector(string name) {
+vector<string> Instructions;
+memory Cell;
+vector<string> memory::ReadToVector(string name)
+{
 
     ifstream instructionsFile(name);
-    vector <string> Instructions;
-    if (!instructionsFile.is_open()) {
+    if (!instructionsFile.is_open())
+    {
         cerr << "Error: Could not open Instructions File." << endl;
     }
     string line;
-    while(getline(instructionsFile, line)){
+    while (getline(instructionsFile, line))
+    {
         string word;
         stringstream ss(line);
-        while (ss >> word){
+        while (ss >> word)
+        {
             Instructions.push_back(word.substr(0, 2));
             Instructions.push_back(word.substr(2, 2));
-            
         }
+    }
+    for (int i=0;i < Instructions.size();i+=2){
+        cout << Instructions[i] << Instructions[i+1]<<  endl;
     }
     instructionsFile.close();
     ALU XY;
     machine h;
+    int op;
     for (int i=0;i<Instructions.size();i+=2){
-        int op;
         if(Instructions[i].substr(0,1)=="B"){
             op =7;
         }
@@ -40,25 +48,38 @@ vector <string> memory::ReadToVector(string name) {
         }
         int r= stoi(Instructions[i].substr(1,1));
         int xy= XY.hextodec(Instructions[i+1]);
+        string value = Cell.getCell(xy);
         switch (op) {
             case 1:
-                h.load1(r,xy);
+                h.load1(r,value);
                 break;
             case 2:
-                h.load2(r,xy);
-                break;
-            case 3:
-                h.store(r,xy);
+                h.load2(r,Instructions[i+1]);
                 break;
         }
     }
+    MemoryRegister coutt;
+    coutt.showregisterr();
+
     return Instructions;
+}
+void memory::setCell(int adress, string value)
+{
+    if (adress >= 0 && adress < 256){
+        Instructions[adress]=value;
+    }
+    else{
+        cout << "invalid address";
+    }
 
 }
-void memory::setCell(int adress, string value){
-    
-}
 
-string memory::getCell(int adress) {
-    
+string memory::getCell(int adress)
+{
+    if (adress >= 0 && adress < 256){
+        return Instructions[adress];
+    }
+    else{
+        cout << "invalid address";
+    }
 }
